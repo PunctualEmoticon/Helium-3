13695f83bd137f7b143f96e49c7132670a9b745c
PunctualEmoticon <quark6789@gmail.com>
2014-05-26 22:31:05 -0700

    Wrote Missile class

--- Helium3/src/equipment/Missile.java
+++ Helium3/src/equipment/Missile.java
@@ -24,10 +24,49 @@
  */
 package equipment;
 
+import helium3.Grid;
+import helium3.Location;
+import java.util.List;
+
 /**
  *
  * @author David Hasegawa
  */
 public class Missile extends Weapon {
+
+    public Missile() {
+        super();
+    }
     
+    /**
+     * Returns the Locations where this Missile may currently attack.  This is a
+     * square around thisLoc, with a starting size of 11x11.  The size of this
+     * square may be increased by upgrade this Missile (each upgrade increases
+     * the square's side length by 4).
+     * 
+     * @param thisGrid the Grid that this Missile is currently in.
+     * @param thisLoc the Location that this Missile is currently in.
+     * @return a List of targetable Locations.
+     */
+    @Override
+    public List<Location> getPossibleTargets(Grid thisGrid, Location thisLoc) {
+        int radius = getUpgradeLevel() * 2 + 5;
+        return thisGrid.getLocationsAround(thisLoc, radius);
+    }
+    
+    /**
+     * Attacks one square, assuming this Missile is armed.  If this Missile is
+     * not armed, then calling this method does nothing.
+     * 
+     * @param thisGrid the Grid that this Missile is currently in.
+     * @param attackLoc the Location to attack.
+     */
+    public void attack(Grid thisGrid, Location attackLoc) {
+        if (!isArmed()) {
+            return;
+        }
+        if (thisGrid.getCell(attackLoc).isOccupied()) {
+            thisGrid.getCell(attackLoc).getVehicle().underAttackBy(this);
+        }
+    }
 }
